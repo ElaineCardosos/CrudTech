8c67fb86c2905021d11f951203dcc3eddf8edce4
Elaine Cardoso <elainecardoso374@gmail.com>
2021-12-13 22:38:51 -0300

    Adicionar arquivos de projeto.

---
+++ CrudTech/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CrudTech.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
